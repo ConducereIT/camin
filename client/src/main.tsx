e7e71748404bcc89bfeac35251c6643e86ef60d5
Cristi Miloiu <miloiuc4@gmail.com>
2024-09-25 11:23:37 +0300

    Add email&password auth and small fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./index.css";
 import Calendar from "./routes/calendar";
 import Login from "./routes/login";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { AuthService } from "@genezio/auth";
+import {GoogleOAuthProvider} from "@react-oauth/google";
+import {AuthService} from "@genezio/auth";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Account from "./routes/account.tsx";
 import Admin from "./routes/admin.tsx";
 import MyAppointments from "./routes/myAppointments.tsx";
-import { PreloaderProvider } from "./components/PreloaderProvider.component.tsx";
+import {PreloaderProvider} from "./components/PreloaderProvider.component.tsx";
 import Preloader from "./components/Preloader.component.tsx";
+import Register from "./routes/register.tsx";
 // import Mentenanta from "./routes/mentenanta.tsx";
 
 AuthService.getInstance().setTokenAndRegion(
@@ -23,37 +24,42 @@ AuthService.getInstance().setTokenAndRegion(
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Calendar />,
+    element: <Calendar/>,
     // element: <Mentenanta />,
   },
   {
     path: "/login",
-    element: <Login />,
+    element: <Login/>,
+    // element: <Mentenanta />,
+  },
+  {
+    path: "/signup",
+    element: <Register/>,
     // element: <Mentenanta />,
   },
   {
     path: "/account",
     // element: <Mentenanta />,
-    element: <Account />,
+    element: <Account/>,
   },
   {
     path: "/admin",
     // element: <Mentenanta />,
-    element: <Admin />,
+    element: <Admin/>,
   },
   {
     path: "/myappointments",
     // element: <Mentenanta />,
-    element: <MyAppointments />,
+    element: <MyAppointments/>,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PreloaderProvider>
-      <Preloader />
+      <Preloader/>
       <GoogleOAuthProvider clientId="1062295332779-684ijgeeas6721n2ekoe71nkcpcqcu7s.apps.googleusercontent.com">
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
       </GoogleOAuthProvider>
     </PreloaderProvider>
   </React.StrictMode>
